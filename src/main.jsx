eac0a6d2a585771ca9e74e772d211dc99ba2ad2d
kartiksharma0047 <kartiksharma55109@gmail.com>
2024-01-11 18:37:35 +0530

    src issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Change file name only here if function which is import is App. 
-import App from "./todo List/Todo.jsx";
+import App from "./Router/Display.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
