a2b5d4b1ee78f2fd03fd0458828912b3f6552c6c
kartiksharma0047 <kartiksharma55109@gmail.com>
2024-01-22 18:36:36 +0530

    Some other changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // Change the location of file
 import App from './E-commerce/FrontPage.jsx'
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// Change file name only here if function which is import is App. 
-import App from "./E-commerce/FrontPage.jsx";
->>>>>>> 1df3c4de411988f5c766900bc4effbd87eea4c9b
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> 1df3c4de411988f5c766900bc4effbd87eea4c9b
