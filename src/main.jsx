1ff8d048df1c9cd48ccae11018b11b762c8f5254
kartiksharma0047 <kartiksharma55109@gmail.com>
2024-05-31 07:49:19 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client'
 // import { Provider } from 'react-redux'
 
 // Change the location of file
-import App from './ReactForm/Form.jsx'
+import App from './E commerce/FrontPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
